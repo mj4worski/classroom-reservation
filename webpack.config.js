cedcbe3351e7645d0cab05dadfab602de0eff861
mj4worski <jaworskimateusz2@gmail.com>
2017-12-17 19:47:49 +0100

    chore: Configure webpack with babel and webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8080
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+
+    plugins: [new HtmlWebpackPlugin()]
+};
