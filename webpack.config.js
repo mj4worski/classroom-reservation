df7c4e64056626a9b398df5f3a1ecfe0e1d85b12
mj4worski <jaworskimateusz2@gmail.com>
2017-12-28 10:54:45 +0100

    chore: Configure bootstrap 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const webpack = require('webpack');
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
 
 module.exports = {
     entry: "./index.js",
@@ -20,11 +24,67 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    'file-loader?name=images/[name].[ext]',
+                    'image-webpack-loader?bypassOnDebug'
+                ]
+            },
+
+            {
+                test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(scss)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader', // translates CSS into CommonJS modules
+                        }, {
+                            loader: 'postcss-loader', // Run post css actions
+                            options: {
+                                plugins() {
+                                    // post css plugins, can be exported to postcss.config.js
+                                    return [
+                                        precss,
+                                        autoprefixer
+                                    ];
+                                }
+                            }
+                        }, {
+                            loader: 'sass-loader' // compiles SASS to CSS
+                        }
+                    ]
+                })
+            },
         ]
     },
 
-    plugins: [new HtmlWebpackPlugin({
-        template:  path.join(__dirname, "index.html")
-    })]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "index.html")
+        }),
+        new ExtractTextPlugin("styles.css"),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            tether: 'tether',
+            Tether: 'tether',
+            'window.Tether': 'tether',
+            Popper: ['popper.js', 'default'],
+            'window.Tether': 'tether',
+        }),
+    ]
 };
