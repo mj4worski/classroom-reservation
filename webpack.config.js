098a4b889bf301ca1967cc432c97cb884a7f691a
mj4worski <jaworskimateusz2@gmail.com>
2018-01-06 15:25:47 +0100

    chore: Update to bootstrap 4 beta.3

--- webpack.config.js
+++ webpack.config.js
@@ -6,84 +6,84 @@ const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 
 module.exports = {
-    entry: "./index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
 
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 8080
-    },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                use: 'url-loader?limit=10000',
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-                use: 'file-loader',
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    'file-loader?name=images/[name].[ext]',
-                    'image-webpack-loader?bypassOnDebug'
-                ]
-            },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          'image-webpack-loader?bypassOnDebug',
+        ],
+      },
 
+      {
+        test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
             {
-                test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader']
+              loader: 'css-loader', // translates CSS into CommonJS modules
+            }, {
+              loader: 'postcss-loader', // Run post css actions
+              options: {
+                plugins() {
+                  // post css plugins, can be exported to postcss.config.js
+                  return [
+                    precss,
+                    autoprefixer,
+                  ];
+                },
+              },
+            }, {
+              loader: 'sass-loader', // compiles SASS to CSS
             },
-            {
-                test: /\.(scss)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader', // translates CSS into CommonJS modules
-                        }, {
-                            loader: 'postcss-loader', // Run post css actions
-                            options: {
-                                plugins() {
-                                    // post css plugins, can be exported to postcss.config.js
-                                    return [
-                                        precss,
-                                        autoprefixer
-                                    ];
-                                }
-                            }
-                        }, {
-                            loader: 'sass-loader' // compiles SASS to CSS
-                        }
-                    ]
-                })
-            },
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "index.html")
+          ],
         }),
-        new ExtractTextPlugin("styles.css"),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            tether: 'tether',
-            Tether: 'tether',
-            'window.Tether': 'tether',
-            Popper: ['popper.js', 'default'],
-        }),
-    ]
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      tether: 'tether',
+      Tether: 'tether',
+      'window.Tether': 'tether',
+      Popper: ['popper.js', 'default'],
+    }),
+  ],
 };
