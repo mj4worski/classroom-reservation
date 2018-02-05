3dd170741e19a5af11089dc295c20209a2671923
mj4worski <jaworskimateusz2@gmail.com>
2018-02-05 20:56:21 +0100

    refactor: apply responsive design for Header/Footer

--- src/client/components/footer/Footer.jsx
+++ src/client/components/footer/Footer.jsx
@@ -4,9 +4,9 @@ import aghLogo from '../../../../assets/images/agh_logo.jpg';
 import './Footer.scss';
 
 export default () => (
-  <footer className="bg-black fixed-bottom footer">
-    <div className="container footer__container">
-      <img src={aghLogo} alt="" />
+  <footer className="cr-footer">
+    <div className="cr-footer__container">
+      <img className="cr-footer__agh-logo" src={aghLogo} alt="" />
       <p className="m-0 text-center text-white small">Copyright &copy; Rezerwacja sal 2017</p>
     </div>
   </footer>
