7829a8e8dfb5d87bda24fa841381a93a00990e78
mj4worski <jaworskimateusz2@gmail.com>
2018-02-24 20:41:32 +0100

    feat: Controlled mode for Registration

--- src/client/components/Registration/Registration.jsx
+++ src/client/components/Registration/Registration.jsx
@@ -1,40 +1,66 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import './Registration.scss';
 
-const Registration = () => {
-  const emailId = 'formEmialRegistration';
-  const passwordId = 'formPasswordRegistration';
-  return (
-    <form className="p-4">
-      <div className="form-group">
-        <label htmlFor={emailId} className="registration-form__label">
-                    Adres Email
-          <input
-            type="email"
-            className="form-control"
-            id={emailId}
-            placeholder="email@example.com"
-          />
-        </label>
-      </div>
-      <div className="form-group">
-        <label htmlFor={passwordId} className="registration-form__label">
-                    Hasło
-          <input
-            type="text"
-            className="form-control"
-            id={passwordId}
-            placeholder="Password"
-          />
-        </label>
+export default class Registration extends PureComponent {
+    state = {
+      email: '',
+      password: '',
+    };
 
-      </div>
-      <button type="submit" className="registration-form__button btn btn-danger">
-          Zarejestruj
-      </button>
-    </form>
-  );
-};
+    onEmailChange = ({ target }) => {
+      this.setState({ email: target.value });
+    };
 
-export default Registration;
+    onPasswordChange = ({ target }) => {
+      this.setState({ password: target.value });
+    };
+
+    onSubmit = () => {
+
+    };
+
+    render() {
+      const { email, password } = this.state;
+      const emailId = 'formEmialRegistration';
+      const passwordId = 'formPasswordRegistration';
+      return (
+        <form className="p-4">
+          <div className="form-group">
+            <label htmlFor={emailId} className="registration-form__label">
+                        Adres Email
+              <input
+                type="email"
+                className="form-control"
+                id={emailId}
+                placeholder="email@example.com"
+                value={email}
+                onChange={this.onEmailChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor={passwordId} className="registration-form__label">
+                        Hasło
+              <input
+                type="text"
+                className="form-control"
+                id={passwordId}
+                placeholder="Password"
+                value={password}
+                onChange={this.onPasswordChange}
+              />
+            </label>
+
+          </div>
+          <button
+            type="submit"
+            className="registration-form__button btn btn-danger"
+            onClick={this.onSubmit}
+          >
+                    Zarejestruj
+          </button>
+        </form>
+      );
+    }
+}
