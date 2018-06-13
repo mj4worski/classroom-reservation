8c305d15c4bd4e8c05e9dc64c5d56c2b52e557af
Mateusz Jaworski <mateusz.jaworski@grandparade.co.uk>
2018-06-13 22:23:36 +0200

    feat: Change visual design for Registration page

--- src/client/components/Registration/Registration.jsx
+++ src/client/components/Registration/Registration.jsx
@@ -2,9 +2,9 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { registration } from './actions';
-
-import './Registration.scss';
+import calendarSvg from '../reservation/calendar.svg';
 import LayoutContent from '../LayoutContent';
+import './Registration.scss';
 
 class Registration extends PureComponent {
     static propTypes = {
@@ -37,42 +37,52 @@ class Registration extends PureComponent {
       const passwordId = 'formPasswordRegistration';
       return (
         <LayoutContent>
-          <form className="p-4">
-            <div className="form-group">
-              <label htmlFor={emailId} className="registration-form__label">
-                        Adres Email
-                <input
-                  type="email"
-                  className="form-control"
-                  id={emailId}
-                  placeholder="email@example.com"
-                  value={email}
-                  onChange={this.onEmailChange}
-                />
-              </label>
+          <div className="registration">
+            <div className="registration-info">
+              <span className="registration-info__title">
+                    Tylko zarejestrowani użytkownicy mogą dokonywać rezerwacji sal
+              </span>
+              <img src={calendarSvg} alt="calendar-icon" className="registration-info__calendar"/>
             </div>
-            <div className="form-group">
-              <label htmlFor={passwordId} className="registration-form__label">
-                        Hasło
-                <input
-                  type="password"
-                  className="form-control"
-                  id={passwordId}
-                  placeholder="Password"
-                  value={password}
-                  onChange={this.onPasswordChange}
-                />
-              </label>
+            <form className="registration-form">
+              <h1 className="registration-form__header">Rejestracja</h1>
+              <h2 className="registration-form__sub-header">Stwórz konto w systemie rezerwacji, lub zaloguj się</h2>
+              <div className="form-group">
+                <label htmlFor={emailId} className="registration-form__label">
+                  Adres Email
+                  <input
+                    type="email"
+                    className="form-control"
+                    id={emailId}
+                    placeholder="email@example.com"
+                    value={email}
+                    onChange={this.onEmailChange}
+                  />
+                </label>
+              </div>
+              <div className="form-group">
+                <label htmlFor={passwordId} className="registration-form__label">
+                  Hasło
+                  <input
+                    type="password"
+                    className="form-control"
+                    id={passwordId}
+                    placeholder="Password"
+                    value={password}
+                    onChange={this.onPasswordChange}
+                  />
+                </label>
 
-            </div>
-            <button
-              type="submit"
-              className="registration-form__button btn btn-danger"
-              onClick={this.onSubmit}
-            >
-              Zarejestruj
-            </button>
-          </form>
+              </div>
+              <button
+                type="submit"
+                className="registration-form__button btn btn-danger"
+                onClick={this.onSubmit}
+              >
+                Zarejestruj
+              </button>
+            </form>
+          </div>
         </LayoutContent>
       );
     }
