2f816784accba0d1ff315d01aecf88d895f7133e
mj4worski <jaworskimateusz2@gmail.com>
2019-07-07 20:23:56 +0200

    refactor: Remove week view

--- src/client/components/calendar/components/Calendar.jsx
+++ src/client/components/calendar/components/Calendar.jsx
@@ -3,17 +3,11 @@ import moment from 'moment';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Month from './Month';
-import Week from './Week';
 import { eventsType } from './types';
 import { fetchReservationsByUserId } from '../actions';
 import arrow from './arrow.svg';
 
 import './Calendar.scss';
-
-const CalendarType = {
-  MONTH: 'month',
-  WEEK: 'week',
-};
 
 const filterEventByMonthAndYear = (events, month, year) =>
   events.filter(({ when }) => when.getMonth() === month && when.getFullYear() === year);
@@ -21,18 +15,15 @@ const filterEventByMonthAndYear = (events, month, year) =>
 class Calendar extends Component {
   static propTypes = {
     events: eventsType,
-    children: PropTypes.node,
     featchReservationRequest: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
     events: [],
-    children: null,
   };
 
   state = {
     month: moment(),
-    calendarType: CalendarType.MONTH,
   };
 
   componentDidMount() {
@@ -47,28 +38,10 @@ class Calendar extends Component {
     this.setState({ month: this.state.month.add(1, 'M') });
   };
 
-  monthView = () => {
-    this.setState({ calendarType: CalendarType.MONTH });
-  };
-
-  weekView = () => {
-    this.setState({ calendarType: CalendarType.WEEK });
-  };
-
-  renderHeaderBottom = children => (children ? <div>{children}</div> : null);
-
   renderHeader = () => (
     <header className="calendar-header">
       <div className="calendar-current-date">
         <h1 className="calendar-current-date__content">{this.state.month.locale('pl').format('MMMM YYYY')}</h1>
-      </div>
-      <div>
-        <button className="btn btn-outline-light" onClick={this.monthView}>
-                Miesiac
-        </button>
-        <button className="btn btn-outline-light" onClick={this.weekView}>
-                Tydzien
-        </button>
       </div>
       <div className="calendar-header-navigation">
         <button className="calendar-header-navigation__button" onClick={this.previous}>
@@ -86,19 +59,17 @@ class Calendar extends Component {
   );
 
   render() {
-    const { month, calendarType } = this.state;
-    const { events, children } = this.props;
+    const { month } = this.state;
+    const { events } = this.props;
     const eventsForCurrentMonth = filterEventByMonthAndYear(
       events,
       month.get('month'),
       month.get('year'),
     );
-    const CalendarContent = calendarType === CalendarType.MONTH ? Month : Week;
     return (
       <div className="calendar">
         {this.renderHeader()}
-        {this.renderHeaderBottom(children)}
-        <CalendarContent
+        <Month
           month={month}
           events={eventsForCurrentMonth}
           currentMonth={month.get('month')}
